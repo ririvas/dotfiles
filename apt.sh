e2cd45f8511a9fc5d6941aa492f2cda5efdf2a40
Ricardo Rivas <rrivas@CHITL3-RRIVAS.mesirow.net>
2026-06-29 10:24:22 -0500

    missing dependencies for plugins in nvim

--- apt.sh
+++ apt.sh
@@ -1,5 +1,8 @@
 apt install gnome-keyring
 apt install python3-minimal
 apt install lazygit
+apt install fd-find
+apt install fzf
+apt install libclang-dev clang
 
 ln -s /usr/bin/python3 /usr/bin/python
