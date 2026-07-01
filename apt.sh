fae1ff2ceae7a8688e56859a6ac536e36b152cfc
Ricardo Rivas <contact@ricardorivas.io>
2026-06-30 19:36:23 -0500

    install go

--- apt.sh
+++ apt.sh
@@ -1,8 +1,13 @@
+
+add-apt-repository ppa:longsleep/golang-backports
+apt update
+
 apt install gnome-keyring
 apt install python3-minimal
 apt install lazygit
 apt install fd-find
 apt install fzf
 apt install libclang-dev clang
+apt install golang
 
 ln -s /usr/bin/python3 /usr/bin/python
